643d8bfbf5c34ed53f15be5b6e43bf4b22a43e5f
JoaoBourganos <84044256+JoaoBourganos@users.noreply.github.com>
2022-12-03 13:29:49 -0300

    Add files via upload

---
+++ Calculadora Simples/script.js
@@ -0,0 +1,25 @@
+function Calcular(op){
+    
+    var valor1 = document.getElementById("valor1");
+    var valor2 = document.getElementById("valor2");
+    var total = document.getElementById("total");
+    var op, resultado;
+
+    if(op == "-"){
+        resultado = Number(valor1.value) - Number(valor2.value);
+        total.innerHTML = resultado;
+    }
+    else if(op == "+"){
+        resultado = Number(valor1.value) +  Number(valor2.value);
+        total.innerHTML = resultado;
+    }
+    else if(op == "/"){
+        resultado = Number(valor1.value) /  Number(valor2.value);
+        total.innerHTML = resultado;
+    }
+    else if(op == "*"){
+        resultado = Number(valor1.value) *  Number(valor2.value);
+        total.innerHTML = resultado;
+    }
+    
+}
